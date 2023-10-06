2e721c1a847db6c0f05568c82e7bf923d09afeee
Erika REDON <erika_redon@hotmail.com>
2023-10-06 19:33:00 +0200

    modifying the twig page in the render

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,6 @@ class HomeController extends AbstractController
 #[Route('/')]
     public function number() : Response
     {
-        $number = rand(0, 100);
-        return $this->render('base.html.twig');
+        return $this->render('home.html.twig');
     }
 }
