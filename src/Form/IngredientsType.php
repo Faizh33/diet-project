4845a309b891321296fe7ccef9b52606ed5c8cf8
Erika REDON <erika_redon@hotmail.com>
2023-10-09 17:43:31 +0200

    created necessary form for the recipe controller of the admin dashboard

---
+++ src/Form/IngredientsType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ingredients;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class IngredientsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', null, [
+                'label' => 'Nom de l\'ingrédient',
+            ])
+            ->add('quantity', null, [
+                'label' => 'Quantité',
+            ])
+            ->add('unity', null, [
+                'label' => 'Unité (kg, L, cuillère à café, ...)',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ingredients::class,
+        ]);
+    }
+}
---
+++ src/Form/StepsType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Steps;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StepsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('description', null, [
+                'label' => 'Description',
+            ])
+            ->add('orderNumber', null, [
+                'label' => 'Etape n°',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Steps::class,
+        ]);
+    }
+}
