661ebef451e4aaf0fdc1d087544a0fd93c5a5017
Erika REDON <erika_redon@hotmail.com>
2023-10-13 15:45:59 +0200

    setting up authentication with make:auth

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/connexion', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Check if there's a login error and set a flash message if needed
+        if ($error) {
+            $this->addFlash('error', 'Invalid credentials. Please try again.');
+        }
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        //throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
