8dcb954aafe52f42b5c2e4745a1af3b170f5b18b
Erika REDON <erika_redon@hotmail.com>
2023-10-13 15:15:35 +0200

    setting up password hashing

--- src/Controller/Admin/UsersCrudController.php
+++ src/Controller/Admin/UsersCrudController.php
@@ -3,12 +3,27 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Users;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormBuilderInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UsersCrudController extends AbstractCrudController
 {
+    private $userPasswordHasher;
+
+    public function __construct(
+        UserPasswordHasherInterface $userPasswordHasher
+    ) {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Users::class;
@@ -21,8 +36,11 @@ class UsersCrudController extends AbstractCrudController
                 ->setLabel('Nom'),
             TextField::new('firstName')
                 ->setLabel('Prénom'),
-            TextField::new('userName')
-                ->setLabel('Nom d\'utilisateur'),
+            EmailField::new('email')
+                ->setLabel('Email'),
+            TextField::new('password')
+                ->setLabel('Mot de passe')
+                ->onlyOnForms(),
             AssociationField::new('diets')
                 ->setLabel('Régimes')
                 ->setFormTypeOptions([
@@ -53,4 +71,40 @@ class UsersCrudController extends AbstractCrudController
                 }),
         ];
     }
+
+    
+    public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    private function addPasswordEventListener(FormBuilderInterface $formBuilder): FormBuilderInterface
+    {
+        return $formBuilder->addEventListener(FormEvents::POST_SUBMIT, $this->hashPassword());
+    }
+
+    private function hashPassword(): \Closure
+    {
+        return function($event) {
+            $form = $event->getForm();
+            if (!$form->isValid()) {
+                return;
+            }
+            $password = $form->get('password')->getData();
+            if ($password === null) {
+                return;
+            }
+    
+            $user = $form->getData();
+            $hash = $this->userPasswordHasher->hashPassword($user, $password);
+            $user->setPassword($hash);
+        };
+    }    
 }
