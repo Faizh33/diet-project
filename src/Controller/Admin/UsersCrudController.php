11550f1c5a22a8e029e09de3955372ad4b604f25
Erika REDON <erika_redon@hotmail.com>
2023-10-16 18:17:47 +0200

    added the plainpassword field in the edit form to modify the password without displaying it first

--- src/Controller/Admin/UsersCrudController.php
+++ src/Controller/Admin/UsersCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Users;
 use Symfony\Component\Form\FormEvents;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use Symfony\Component\Form\FormBuilderInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -13,6 +14,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType; // Ajout du namespace
 
 class UsersCrudController extends AbstractCrudController
 {
@@ -31,48 +33,51 @@ class UsersCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        return [
-            TextField::new('lastName')
-                ->setLabel('Nom'),
-            TextField::new('firstName')
-                ->setLabel('Prénom'),
-            EmailField::new('email')
-                ->setLabel('Email'),
-            TextField::new('password')
-                ->setLabel('Mot de passe')
-                ->onlyOnForms(),
+        $fields = [
+            TextField::new('lastName')->setLabel('Nom'),
+            TextField::new('firstName')->setLabel('Prénom'),
+            EmailField::new('email')->setLabel('Email'),
             AssociationField::new('diets')
-                ->setLabel('Régimes')
-                ->setFormTypeOptions([
-                    'multiple' => true,
-                    'by_reference' => false,
-                ])
-                ->formatValue(function ($value, $entity) {
-                    $diets = $entity->getDiets();
-                    $dietNames = [];
-                    foreach ($diets as $diet) {
-                        $dietNames[] = $diet->getName();
-                    }
-                    return implode(', ', $dietNames);
-                }),
+            ->setLabel('Régimes')
+            ->setFormTypeOptions([
+                'multiple' => true,
+                'by_reference' => false,
+            ])
+            ->formatValue(function ($value, $entity) {
+                $diets = $entity->getDiets();
+                $dietNames = [];
+                foreach ($diets as $diet) {
+                    $dietNames[] = $diet->getName();
+                }
+                return implode(', ', $dietNames);
+            }),
             AssociationField::new('allergens')
-                ->setLabel('Allergènes')
-                ->setFormTypeOptions([
-                    'multiple' => true,
-                    'by_reference' => false,
-                ])
-                ->formatValue(function ($value, $entity) {
-                    $allergens = $entity->getAllergens();
-                    $allergenNames = [];
-                    foreach ($allergens as $allergen) {
-                        $allergenNames[] = $allergen->getName();
-                    }
-                    return implode(', ', $allergenNames);
-                }),
+            ->setLabel('Allergènes')
+            ->setFormTypeOptions([
+                'multiple' => true,
+                'by_reference' => false,
+            ])
+            ->formatValue(function ($value, $entity) {
+                $allergens = $entity->getAllergens();
+                $allergenNames = [];
+                foreach ($allergens as $allergen) {
+                    $allergenNames[] = $allergen->getName();
+                }
+                return implode(', ', $allergenNames);
+            }),
         ];
+
+        if ($pageName === Crud::PAGE_NEW) {
+            $fields[] = TextField::new('password')->setLabel('Mot de passe');
+        }
+
+        if ($pageName === Crud::PAGE_EDIT) {
+            $fields[] = TextField::new('plainPassword')->setLabel('Nouveau mot de passe');
+        }
+
+        return $fields;
     }
 
-    
     public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
     {
         $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
@@ -92,19 +97,19 @@ class UsersCrudController extends AbstractCrudController
 
     private function hashPassword(): \Closure
     {
-        return function($event) {
+        return function ($event) {
             $form = $event->getForm();
             if (!$form->isValid()) {
                 return;
             }
-            $password = $form->get('password')->getData();
-            if ($password === null) {
-                return;
-            }
-    
+
             $user = $form->getData();
-            $hash = $this->userPasswordHasher->hashPassword($user, $password);
-            $user->setPassword($hash);
+            $plainPassword = $form->get('plainPassword')->getData();
+            if ($plainPassword !== null) {
+                $hash = $this->userPasswordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hash);
+                $user->eraseCredentials();
+            }
         };
-    }    
+    }
 }
